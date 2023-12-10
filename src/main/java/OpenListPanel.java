0c2c3ce432bf665280f2380b0cc0c32d9ddc6c7f
Piotr Zapiór <piotrzapior0@gmail.com>
2023-12-10 01:19:30 +0100

    update

--- src/main/java/OpenListPanel.java
+++ src/main/java/OpenListPanel.java
@@ -1,4 +1,7 @@
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
 import javax.swing.*;
+import java.awt.*;
 
 public class OpenListPanel extends JPanel {
     private JPanel namePanel;
@@ -18,6 +21,16 @@ public class OpenListPanel extends JPanel {
     private JLabel liftsLabel;
 
     public OpenListPanel(Resort resort){
+        setSize(800,100);
+        setPreferredSize(new Dimension(800, 100));
+        setLayout(new FormLayout("pref:grow, pref, pref, pref, pref, pref", "pref"));
+        CellConstraints cc = new CellConstraints();
+        add(namePanel,cc.xy(1,1));
+        add(snowLastPanel, cc.xy(2,1));
+        add(currSnowPanel, cc.xy(3,1));
+        add(trailsPanel, cc.xy(4,1));
+        add(liftsPanel, cc.xy(5,1));
+        add(favoritePanel, cc.xy(6,1));
         nameLabel.setText(resort.name());
         updateTimeLabel.setText(resort.updateTime());
         snowLastLabel.setText(resort.snowLast24());
