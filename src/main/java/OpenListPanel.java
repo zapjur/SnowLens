32ad104b082dfe4273dec0ab23b4369afa7fac95
Piotr Zapiór <piotrzapior0@gmail.com>
2023-12-12 18:05:21 +0100

    update

--- src/main/java/OpenListPanel.java
+++ src/main/java/OpenListPanel.java
@@ -24,13 +24,47 @@ public class OpenListPanel extends JPanel {
     public OpenListPanel(Resort resort){
         setSize(800,100);
         setPreferredSize(new Dimension(800, 100));
-        setLayout(new MigLayout("wrap 6", "[][][][][][]", "100px"));
-        add(namePanel, "cell 1 1");
-        add(snowLastPanel, "cell 2 1");
-        add(currSnowPanel, "cell 3 1");
-        add(trailsPanel, "cell 4 1");
-        add(liftsPanel, "cell 5 1");
-        add(favoritePanel, "cell 6 1");
+
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+        nameLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        updateTimeLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        nameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        updateTimeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        namePanel.setLayout(new BoxLayout(namePanel, BoxLayout.PAGE_AXIS));
+        namePanel.setPreferredSize(new Dimension(200, 100));
+        namePanel.add(nameLabel);
+        namePanel.add(updateTimeLabel);
+        add(namePanel);
+
+        snowLastPanel.setPreferredSize(new Dimension(100, 100));
+        add(snowLastPanel);
+
+        currSnowLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        snowTypeLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        currSnowLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        snowTypeLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        currSnowPanel.setLayout(new BoxLayout(currSnowPanel, BoxLayout.PAGE_AXIS));
+        currSnowPanel.setPreferredSize(new Dimension(200, 100));
+        currSnowPanel.add(currSnowLabel);
+        currSnowPanel.add(snowTypeLabel);
+        add(currSnowPanel);
+
+        trailsDistLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        trailsPerLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+        trailsDistLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        trailsPerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        trailsPanel.setLayout(new BoxLayout(trailsPanel, BoxLayout.PAGE_AXIS));
+        trailsPanel.setPreferredSize(new Dimension(200, 100));
+        trailsPanel.add(trailsDistLabel);
+        trailsPanel.add(trailsPerLabel);
+        add(trailsPanel);
+
+        liftsPanel.setPreferredSize(new Dimension(100, 100));
+        add(liftsPanel);
+
+        favoritePanel.setPreferredSize(new Dimension(100, 100));
+        add(favoritePanel);
 
         nameLabel.setText(resort.name());
         updateTimeLabel.setText(resort.updateTime());
